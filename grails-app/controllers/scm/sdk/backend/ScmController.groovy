018a800603bb0ef28a9b3399300105ac8b7bd27f
Darwis <darwisnarvaezdev@gmail.com>
2023-01-17 00:26:33 -0300

    Building responses structure

--- grails-app/controllers/scm/sdk/backend/ScmController.groovy
+++ grails-app/controllers/scm/sdk/backend/ScmController.groovy
@@ -18,11 +18,10 @@ class ScmController {
         // Get from user if we destroy the folder and its files after the SCM
         println "destroy scm folder? ${params.destroyFolder}"
         // Clone the repo into path
-        println "Get: ${scmService.gitTargetFolder}/${yamlService.getFilename()}"
         def result = scmService.cloneRepositoryToTemp()
         if( result ){
             if( 'Success' != result.results ){
-                if( result.results.contains("Authentication") ){
+                if( result.results.contains("Authentication") || result.results.contains("authorized") ){
                     render "You are trying to access a private repo, please provide the property 'git.authToken'"
                 }else{
                     render result.results
